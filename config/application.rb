3f2f3906320049ce85749a9c63c84bd51215468d
Your Name <you@example.com>
2024-01-25 12:29:57 +0530

    first commit

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative "boot"
+
+require "rails/all"
+
+
+Bundler.require(*Rails.groups)
+
+module Ecomm
+  class Application < Rails::Application
+  
+    config.load_defaults 7.1
+
+    config.autoload_lib(ignore: %w(assets tasks))
+
+   
+  end
+end
